37dd0c8aff85f8b88f5c43798586b63f5ce7ac48
Benjamin Hughes <behu@kea.dk>
2021-03-03 13:47:02 +0100

    asd

--- src/main/java/com/example/demo/models/CityWeather.java
+++ src/main/java/com/example/demo/models/CityWeather.java
@@ -9,7 +9,6 @@ import java.net.URL;
 
 public class CityWeather {
     public static WeatherData getWeatherData(String cityName, String apiKey) throws IOException {
-        // "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2135c3f7f652abb768bf5a7fd30d743c"
         URL weatherUrl= new URL("http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + cityName + "&APPID=" + apiKey);
         BufferedReader inputFromWeatherUrl = new BufferedReader(new InputStreamReader(weatherUrl.openStream()));
         WeatherData weatherData = new Gson().fromJson(inputFromWeatherUrl, WeatherData.class);
